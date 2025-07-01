ecb4a72a761941d24c601be3df5e8697ba413b82
Tina-idk <d5hztina@gmail.com>
2025-07-01 21:02:26 +0100

    Update guessing words.py

--- guessing words.py
+++ guessing words.py
@@ -2,53 +2,59 @@ import random
 import pandas as pd
 import streamlit as st
 st.title("Game: Word Guessing Game")
-st.write("Game Rule: 1. Try to guess the word in limited trials(10)")
+st.write("Game Rule: ")
+st.write("           1. Try to guess the word in limited trials(10)")
 st.write("           2. Enter one letter each time")
 st.write("           3. If the letter is in the word, it will be displayed")
 st.write("           4. If the letter is not in the word, you will lose a life")
 st.write("Good luck!")
 
 words = pd.read_csv('A2_1000_Vocabulary.csv')
-words = words[['Word', 'Meaning']]
-words = words.dropna(subset=['Word', 'Meaning'])
+words = words[['Word', 'Meaning']]. dropna
 word_list = {word: Meaning for word, Meaning in zip(words['Word'], words['Meaning'])}
 
-while True:
-    life = 10
-    word = random.choice(list(word_list.keys()))
-    paraphrase = word_list.get(word)[0]
-    length = len(word)
-    guess = ['_'] * length
-    st.write(' '.join(guess))
+if "word" not in st.session_state:
+    st.session_state.word = random.choice(list(word_list.keys()))
+    st.session_state.meaning = word_list[st.session_state.word]
+    st.session_state.guess = ['_'] * len(st.session_state.word)
+    st.session_state.life = 10
+    st.session_state.letters_guessed = []
 
-    i = 0
-    while '_' in guess:
-        letter = st.text_input('Guess a letter: ', key=f"guess_input_{i}")
-        i += 1
-        if not letter.isalpha():
-            st.warning('Please enter a letter!')
-            continue
-        if letter in word:
-            for i, c in enumerate(word):
-                if c == letter:
-                    guess[i] = letter
-            st.write(guess)
-        else:
-            st.write('The letter is not in the word.')
-            life -= 1
-            st.write(f'Life - 1. Life: {life}')
-            st.write(guess)
-        if life == 0:
-            st.error('Game over!')
-            st.write(f'The answer: {word}')
-            st.write(f'Meaning: {Meaning}')
-            break
+st.write("Word:", ' '.join(st.session_state.guess))
+st.write(f"Lives remaining: {st.session_state.life}")
+st.write(f"Letters guessed: {', '.join(st.session_state.letters_guessed)}")
+
+letter = st.text_input("Guess a letter: ", max_chars=1, key="current_guess")
+
+if st.button("Submit Guess"):
+    if not letter.isalpha():
+        st.warning("Please enter a valid letter.")
+    elif letter in st.session_state.letters_guessed:
+        st.warning("You've already guessed that letter.")
     else:
-        st.success('Congratulations, you win!')
-        st.write(f'The answer: {word}')
-        st.write(f'Meaning: {Meaning}')
+        st.session_state.letters_guessed.append(letter)
 
-    if st.button("Continue"):
-        continue
-    if st.button("Quit"):
-        break
+        if letter in st.session_state.word:
+            for i, c in enumerate(st.session_state.word):
+                if c == letter:
+                    st.session_state.guess[i] = letter
+            st.success("Correct guess!")
+        else:
+            st.session_state.life -= 1
+            st.error("Wrong guess!")
+
+if '_' not in st.session_state.guess:
+    st.success("🎉 You win!")
+    st.balloons()
+    st.write(f"The word was: **{st.session_state.word}**")
+    st.write(f"Meaning: {st.session_state.meaning}")
+    if st.button("Play Again"):
+        st.session_state.clear()
+
+elif st.session_state.life == 0:
+    st.error("💀 Game over!")
+    st.write(f"The word was: **{st.session_state.word}**")
+    st.write(f"Meaning: {st.session_state.meaning}")
+    if st.button("Try Again"):
+        st.session_state.clear()
+    
