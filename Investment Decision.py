b17149ee38d09b42cec635b2ebff556415cc08e1
Tina-idk <d5hztina@gmail.com>
2025-07-08 10:24:06 +0100

    Update Investment Decision.py

--- Investment Decision.py
+++ Investment Decision.py
@@ -13,24 +13,11 @@ st.markdown("""
     @media print {
         div[data-testid="stButton"],
         div[data-testid="stDownloadButton"],
-        div[data-testid="stFileUploader"],
-        div[data-testid="stMultiSelect"],
-        div[data-testid="stTextArea"],
-        div[data-testid="stTextInput"] {
+        div[data-testid="stFileUploader"] {
             display: none !important;
         }
-
         header, footer, .stSidebar {
             display: none !important;
-        }
-
-        div[role="listbox"],
-        div[role="combobox"] {
-            display: none !important;
-        }
-
-        @page {
-            margin: 10mm;
         }
     }
     </style>
